d5885d405d1e7acca69b699adc7c0b90b86b0c21
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2024-11-21 23:44:18 -0600

    third commit

--- script.js
+++ script.js
@@ -36,12 +36,94 @@
 //       console.log("All tasks done!");
 //     });
  
-const faultyTask = () => 
-    new Promise((resolve, reject) => 
-        setTimeout(() => reject("Something went really wrong!"), 1500)
-    )
+// const faultyTask = () => 
+//     new Promise((resolve, reject) => 
+//         setTimeout(() => reject("Something went really wrong!"), 1500)
+//     )
 
-faultyTask()
-    .then((result) => console.log(result))
-    .catch((error) => console.error("Error:", error)); 
+// faultyTask()
+//     .then((result) => console.log(result))
+//     .catch((error) => console.error("Error:", error)); 
+
+
+// const pizzaPromise = new Promise((resolve, reject) => {
+//     const ingredientsAvailable = true; // Simulate success / failure 
+
+//     setTimeout(() => {
+//         if (ingredientsAvailable) {
+//             resolve("Pizza is ready!"); 
+//         } else {
+//             reject("Sorry, no pizza today.")
+//         }
+//     }, 2000); // simulate time for making the pizza 
+// })
+
+// pizzaPromise
+//     .then((message) => console.log(message)) //handle success
+//     .catch((error) => console.error(error)) //handle failure 
+
+
+// const homeworkPromise = new Promise((resolve, reject) => {
+//     const homeworkSubmitted = true; //simulate if the homework is submitted 
+
+//     setTimeout(() => {
+//         if(homeworkSubmitted) {
+//             resolve("homework graded!")
+//         } else {
+//             reject("homework not submitted!")
+//         }
+//     }, 3000); //simulate time for grading homework 
+// })
+
+// homeworkPromise
+//     .then((result) => console.log(result)) // handle success. Method of the promise object used to define what should happen after a promise is resolved (fulfilled). 
+//     .catch((error) => console.error(error)) // handle failure 
+
+// fetchData()
+//     .then((data) => Process(data))
+//     .then((processedData) => save(processedData))
+//     .catch((error) => handleError(error))
+
+// async function handleData() {
+//     try {
+//         const data = await fetchData(); 
+//         const processedData = await process(data); 
+//         await save(processedData); 
+//     } catch (error) {
+//         handleError(error); 
+//     }
+// }
+
+function walkDog() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("you walk the dog"); 
+        }, 1500)
+    })
+}
+
+function cleanKitchen() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("you cleaned the kitchen"); 
+        }, 2500)
+    })
+}
+
+function takeOutTrash() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("you took out the trash");  
+        }, 1000)
+    })
+}
+
+walkDog()       
+    .then(value => {console.log(value); return cleanKitchen()})
+    .then(value => {console.log(value); return takeOutTrash()})
+    .then(value => {console.log(value); 
+    console.log("all chores are complete!")})
+    .catch((error) => console.error(error))
+
+
   
